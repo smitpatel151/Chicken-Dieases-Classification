c4b51014f07726840b27df721059e3fafaa9a5f6
Smit Patel <smitpatel@Smits-MacBook-Pro.local>
2023-08-29 01:44:43 -0400

    folder structure added

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "test.py"
+]
+
+for filePath in list_of_files:
+    filePath = Path(filePath)
+    fileDir, fileName = os.path.split(filePath)
+
+    if fileDir !="":
+        os.makedirs(fileDir, exist_ok=True)
+        logging.info("Creating directory: ({}) for the file ({})".format(fileDir, fileName))
+
+    if (not os.path.exists(filePath)) or (os.path.getsize(filePath)==0):
+        with open(filePath,"w") as f:
+            pass
+            logging.info("Creating empty file: ({})".format(fileName))
+
+    else:
+        logging.info("{} is already exists".format(fileName))
+
+
